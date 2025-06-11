2b0ea6e4c49b6a3b81aa39a60b1408dad27fa02f
Junus <j.khalek11@gmail.com>
2025-06-11 13:21:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 session_start();
 
-// Überprüfen, ob der Benutzer bereits eingeloggt ist
+// CPrüft ob der Benutzer Angemeldet ist
 if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
-    // Wenn der Benutzer eingeloggt ist, leiten wir ihn zum Ticketsystem weiter
     header("Location: ticketsystem.php");
     exit();
 } else {
-    // Wenn der Benutzer noch nicht eingeloggt ist, leiten wir ihn zur Login-Seite weiter
     header("Location: login.php");
     exit();
 }
